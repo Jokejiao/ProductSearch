9e051cd7eb2296a6b0a85886cedce6bf08ec9d0a
Alex <Jokejiao@gmail.com>
2025-03-10 15:00:30 +1300

    Create Application class as Hilt entry point

---
+++ app/src/main/java/nz/co/thewarehouse/productsearch/ProductSearchApplication.kt
@@ -0,0 +1,8 @@
+package nz.co.thewarehouse.productsearch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class ProductSearchApplication : Application()
