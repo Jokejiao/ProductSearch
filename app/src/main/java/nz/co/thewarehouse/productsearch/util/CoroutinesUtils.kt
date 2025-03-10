88f12af99a1cc3e5bc6e83daa89b568a4fe012cd
Alex <Jokejiao@gmail.com>
2025-03-10 15:03:43 +1300

    Create CoroutinesUtils.kt

---
+++ app/src/main/java/nz/co/thewarehouse/productsearch/util/CoroutinesUtils.kt
@@ -0,0 +1,7 @@
+package nz.co.thewarehouse.productsearch.util
+
+import kotlinx.coroutines.flow.SharingStarted
+
+private const val StopTimeoutMillis: Long = 5000
+
+val WhileUiSubscribed: SharingStarted = SharingStarted.WhileSubscribed(StopTimeoutMillis)
