fc768edaa3796a341bf675ceb2e585bfe438c6a6
Alex <Jokejiao@gmail.com>
2025-03-10 15:03:24 +1300

    Implement the searchProducts API for searching products

--- app/src/main/java/nz/co/thewarehouse/productsearch/data/DefaultProductRepository.kt
+++ app/src/main/java/nz/co/thewarehouse/productsearch/data/DefaultProductRepository.kt
@@ -7,17 +7,18 @@ import nz.co.thewarehouse.productsearch.data.source.network.NetworkDataSource
 import nz.co.thewarehouse.productsearch.di.IoDispatcher
 import javax.inject.Inject
 
+
 class DefaultProductRepository @Inject constructor(
     private val networkDataSource: NetworkDataSource,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ProductRepository {
 
-    override suspend fun searchProducts(query: String): List<UiProduct> =
+    override suspend fun searchProducts(query: String): List<Product> =
         withContext(dispatcher) {
             runCatching {
                 networkDataSource.searchProducts(query).toExternal()
             }.onFailure { e ->
-                Log.e("Repository", "Refresh failed: ${e.message}")
+                Log.e("Repository", "Search products failed: ${e.message}")
             }.getOrElse {
                 emptyList()
             }
--- app/src/main/java/nz/co/thewarehouse/productsearch/data/source/network/ProductNetworkDataSource.kt
+++ app/src/main/java/nz/co/thewarehouse/productsearch/data/source/network/ProductNetworkDataSource.kt
@@ -1,11 +1,13 @@
 package nz.co.thewarehouse.productsearch.data.source.network
 
+import nz.co.thewarehouse.productsearch.api.SearchService
 import javax.inject.Inject
 
+
 class ProductNetworkDataSource @Inject constructor(
+    private val searchService: SearchService
 ) : NetworkDataSource {
 
-    override suspend fun searchProducts(query: String): List<NetworkProduct> {
-        return emptyList()
-    }
+    override suspend fun searchProducts(query: String): List<NetworkProduct> =
+        searchService.searchProducts(query, 10).products
 }
